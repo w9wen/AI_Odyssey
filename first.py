3351275c41b533cb427aa16161fa6994987101c5
w9wen(mac) <william.wen@gmail.com>
2025-11-10 22:11:51 +0800

    first

--- first.py
+++ first.py
@@ -1,3 +1,14 @@
 import numpy as np
 
 print("Hello, World!")
+
+data = [1, 2, 3, 4, 5]
+array = np.array(data)
+print("Numpy array:", array)
+print("Array mean:", np.mean(array))
+
+multiplied_array = array * 10
+print("Multiplied array:", multiplied_array)
+
+add_array = array + 100
+print("Array after addition:", add_array)
